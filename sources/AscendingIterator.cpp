3dc8317a210e581ff160ad858523a5250df1ad05
Lidor Keren Yeshuah <118388214+LidorKY@users.noreply.github.com>
2023-05-30 17:23:25 +0300

    Delete sources directory

--- sources/AscendingIterator.cpp
+++
@@ -1,6 +0,0 @@
-#include "AscendingIterator.hpp"
-#include "MagicalContainer.h"
-
-AscendingIterator::AscendingIterator(MagicalContainer container) {}
-int AscendingIterator::*begin() {}
-int AscendingIterator::*end() {}
