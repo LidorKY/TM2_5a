a7b30921b932fba5041ceee12f2fb0c35807b2f0
Lidor Keren Yeshuah <118388214+LidorKY@users.noreply.github.com>
2023-06-01 10:22:06 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -64,6 +64,29 @@ TEST_CASE("Check the class MagicalContainer")
         CHECK(iter == iter.end());
         CHECK_NOTHROW(++iter);
         CHECK(iter == iter.end());
+
+        // Additional checks to test AscendingIterator's copy constructor and assignment operators
+
+        MagicalContainer::AscendingIterator iterCopy(iter);
+        CHECK_EQ(*iterCopy, 25);
+        CHECK_NOTHROW(++iterCopy);
+        CHECK(iterCopy == iterCopy.end());
+
+        MagicalContainer::AscendingIterator iterCopy2 = iter;
+        CHECK_EQ(*iterCopy2, 25);
+        CHECK_NOTHROW(++iterCopy2);
+        CHECK(iterCopy2 == iterCopy2.end());
+
+        CHECK(iter != iter.end()); // Check operator!= for inequality
+        CHECK(iter.end() != iter); // Check operator!= for inequality (reversed)
+
+        // Check operator>
+        CHECK(iter > iterCopy);
+        CHECK(!(iterCopy > iter));
+
+        // Check operator<
+        CHECK(iterCopy < iter);
+        CHECK(!(iter < iterCopy));
     }
 
     SUBCASE("SideCrossIterator")
@@ -91,6 +114,29 @@ TEST_CASE("Check the class MagicalContainer")
         CHECK(iter == iter.end());
         CHECK_NOTHROW(++iter);
         CHECK(iter == iter.end());
+
+        // Additional checks to test SideCrossIterator's copy constructor and assignment operators
+
+        MagicalContainer::SideCrossIterator iterCopy(iter);
+        CHECK_EQ(*iterCopy, 9);
+        CHECK_NOTHROW(++iterCopy);
+        CHECK(iterCopy == iterCopy.end());
+
+        MagicalContainer::SideCrossIterator iterCopy2 = iter;
+        CHECK_EQ(*iterCopy2, 9);
+        CHECK_NOTHROW(++iterCopy2);
+        CHECK(iterCopy2 == iterCopy2.end());
+
+        CHECK(iter != iter.end()); // Check operator!= for inequality
+        CHECK(iter.end() != iter); // Check operator!= for inequality (reversed)
+
+        // Check operator>
+        CHECK(iter > iterCopy);
+        CHECK(!(iterCopy > iter));
+
+        // Check operator<
+        CHECK(iterCopy < iter);
+        CHECK(!(iter < iterCopy));
     }
 
     SUBCASE("PrimeIterator")
@@ -114,5 +160,24 @@ TEST_CASE("Check the class MagicalContainer")
         CHECK(iter == iter.end());
         CHECK_NOTHROW(++iter);
         CHECK(iter == iter.end());
+
+        // Additional checks to test PrimeIterator's copy constructor and assignment operators
+
+        MagicalContainer::PrimeIterator iterCopy(iter);
+        CHECK(iterCopy == iterCopy.end());
+
+        MagicalContainer::PrimeIterator iterCopy2 = iter;
+        CHECK(iterCopy2 == iterCopy2.end());
+
+        CHECK(iter != iter.end()); // Check operator!= for inequality
+        CHECK(iter.end() != iter); // Check operator!= for inequality (reversed)
+
+        // Check operator>
+        CHECK(iter > iterCopy);
+        CHECK(!(iterCopy > iter));
+
+        // Check operator<
+        CHECK(iterCopy < iter);
+        CHECK(!(iter < iterCopy));
     }
 }
